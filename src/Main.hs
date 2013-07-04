d123803c8b944223b52eab63ff49b8106bd65e2c
Anton Dubovik <gunner.kade@gmail.com>
2013-07-05 00:02:32 +0300

    Delayed newlines.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.Char
 import Data.List
 import Data.Tree
 
@@ -7,6 +8,36 @@ main :: IO ()
 main = do
   input <- getContents
   putStrLn $ format input
+
+format :: String -> String
+format = tabify False 0 .
+         unwords .
+         words
+  where
+    tabify _ _ []  = []
+    tabify _ _ [c] = [c]
+    tabify nl n (c:str)
+      | c `elem` opens = c : tabify True (n+1) str
+      | c `elem` delim = c : tabify True n str
+      | c `elem` close = c : tabify False (n-1) str
+      | nl             = condTabs nl n ++ skipSpaces False n (c:str)
+      | otherwise      = c : tabify nl n str
+
+    skipSpaces nl n str = case dropWhile isSpace str of
+      (c:cs) -> c : tabify nl n cs
+      [] -> []
+
+    condTabs nl n
+      | nl = tabs n
+      | otherwise = ""
+
+    tabs n = '\n' : replicate (2*n) ' '
+
+    opens = "<{(["
+    close = ">})]"
+    delim = ",;"
+
+-- | Demonstration
 
 ppTree :: Show a => Tree a -> String
 ppTree (Node a ch) = "(" ++ show a ++ "," ++ "[" ++ intercalate "," (map ppTree ch) ++ "]" ++ ")"
@@ -17,30 +48,13 @@ mkTree = unfoldTree f 0
     f n | n <= 4    = (n,[n+1..n+3])
         | otherwise = (n,[])
 
-format :: String -> String
-format = tabify 0 .
-         unwords .
-         words
-  where
-    tabify _ [] = []
-    tabify _ [c] = [c]
-    tabify n (c:c':str)
-      | c `elem` opens && c' `elem` opens  = c : tabify (n+1) (c':str)
-    tabify n (c:str)
-      | c `elem` opens = c : (tabs (n+1)) ++ tabify (n+1) str
-      | c `elem` delim = c : (tabs n) ++ tabify n str
-      | c `elem` close = c : tabify (n-1) str
-      | otherwise      = c : tabify n str
-
-    tabs n = '\n' : replicate (2*n) ' '
-
-    opens = "<{(["
-    close = ">})]"
-    delim = ",;"
-
 test :: IO ()
 test = do
-  putStrLn "Records"
+  let underlineLn str = do
+        putStrLn str
+        putStrLn (replicate (length str) '=')
+  
+  underlineLn "Derived show"
   putStrLn $ format $ show mkTree
-  putStrLn "Prettyprint"
+  underlineLn "Prettyprint"
   putStrLn $ format $ ppTree mkTree
